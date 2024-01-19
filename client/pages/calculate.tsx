4edff60cd3df590f6fd0ee789f12663870feb0f9
yh13431 <thiayihong@gmail.com>
2024-01-19 11:29:43 +0800

    more calculator format editing

--- client/pages/calculate.tsx
+++ client/pages/calculate.tsx
@@ -2,6 +2,7 @@ import { useSession } from 'next-auth/react';
 import React, { useState } from 'react';
 import { Result } from '@/types/types';
 import ReactLoading from 'react-loading';
+import Image from 'next/image';
 
 
 export default function Calculate() {
@@ -40,69 +41,82 @@ export default function Calculate() {
 
 
     return (
-      <div className={`min-h-screen flex flex-col items-center justify-center bg-gray-600 relative ${loading ? 'opacity-20' : ''}`} style={{ backgroundImage: "url('calculate.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className={`min-h-screen flex items-center justify-center bg-gray-600 ${loading ? 'opacity-20' : ''}`}>
         {loading && (
-            <div className="loading-overlay w-full h-full flex items-center justify-center absolute top-0 left-0 z-50">
+            <div className="loading-overlay w-full h-full flex items-center justify-center absolute top-0 left-0">
                 <ReactLoading type="spin" color="#000" height={50} width={50} />
             </div>
         )}
-        <div className="text-center mt-12">
-            <h1 className="text-7xl font-bold mb-8 text-gray-100">Nutrition Calculator</h1>
-            <textarea
-                aria-label="Enter recipe ingredients"
-                className="border border-gray-700 p-10 rounded-md w-full resize-y bg-gray-200"
-                rows={5}
-                placeholder="Enter your ingredients or recipes along with their serving sizes."
-                value={ingredients}
-                onChange={(e) => setIngredients(e.target.value)}
+        <div className="flex-shrink-0" style={{ flexBasis: '50%', minWidth: '60%' }}>
+            <Image
+                src="/calculate.jpg"
+                alt="Background Image"
+                layout="fixed"
+                width={1200}
+                height={800}
+                objectFit="cover"
+                objectPosition="left"
             />
-            <button
-                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 transition-transform duration-300 ease-in-out transform hover:scale-110 mt-4 ${
-                    !session ? 'cursor-not-allowed opacity-50' : ''
-                }`}
-                onClick={session ? handleCalculate : undefined}
-                disabled={!session || loading}
-            >
-                {loading ? 'Calculating...' : session ? 'Calculate' : 'Log In to Calculate'}
-            </button>
         </div>
-        <div className="text-center mt-8">
-            {result && (
-                <>
-                    <div className='overflow-x-auto'>
-                        <table className="min-w-full bg-gray-200 border border-gray-300 divide-y divide-gray-300 text-xl">
-                        <thead className="bg-gray-300">
-                            <tr>
-                            <th className="px-6 py-3 text-center font-semibold">Nutrient</th>
-                            <th className="px-6 py-3 text-center font-semibold">Amount</th>
-                            </tr>
-                        </thead>
-                            <tbody className='bg-gray-100'>
-                                {result?.totals && (
-                                    <>
-                                        <tr>
-                                            <td className="border px-6 py-3">Calories</td>
-                                            <td className="border px-6 py-3">{result.totals.total_calories}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="border px-6 py-3">Carbohydrates (g)</td>
-                                            <td className="border px-6 py-3">{result.totals.total_carbohydrates}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="border px-6 py-3">Fats (g)</td>
-                                            <td className="border px-6 py-3">{result.totals.total_fats}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="border px-6 py-3">Protein (g)</td>
-                                            <td className="border px-6 py-3">{result.totals.total_protein}</td>
-                                        </tr>
-                                    </>
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
-                </>
-            )}
+        <div className='flex-col'>
+            <div className="text-center flex-1 p-20">
+                <h1 className="text-7xl font-bold mb-8 text-gray-100">Nutrition Calculator</h1>
+                <textarea
+                    aria-label="Enter recipe ingredients"
+                    className="border border-gray-700 p-10 rounded-md w-full resize-y bg-gray-200"
+                    rows={5}
+                    placeholder="Enter your ingredients or recipes along with their serving sizes."
+                    value={ingredients}
+                    onChange={(e) => setIngredients(e.target.value)}
+                />
+                <button
+                    className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 transition-transform duration-300 ease-in-out transform hover:scale-110 mt-4 ${
+                        !session ? 'cursor-not-allowed opacity-50' : ''
+                    }`}
+                    onClick={session ? handleCalculate : undefined}
+                    disabled={!session || loading}
+                >
+                    {loading ? 'Calculating...' : session ? 'Calculate' : 'Log In to Calculate'}
+                </button>
+            </div>
+            <div className="text-center m-8">
+                {result && (
+                    <>
+                        <div className=''>
+                            <table className="min-w-full bg-gray-200 border border-gray-300 divide-y divide-gray-300 text-xl">
+                            <thead className="bg-gray-300">
+                                <tr>
+                                <th className="px-6 py-3 text-center font-semibold">Nutrient</th>
+                                <th className="px-6 py-3 text-center font-semibold">Amount</th>
+                                </tr>
+                            </thead>
+                                <tbody className='bg-gray-100'>
+                                    {result?.totals && (
+                                        <>
+                                            <tr>
+                                                <td className="border px-6 py-3">Calories</td>
+                                                <td className="border px-6 py-3">{result.totals.total_calories}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border px-6 py-3">Carbohydrates (g)</td>
+                                                <td className="border px-6 py-3">{result.totals.total_carbohydrates}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border px-6 py-3">Fats (g)</td>
+                                                <td className="border px-6 py-3">{result.totals.total_fats}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border px-6 py-3">Protein (g)</td>
+                                                <td className="border px-6 py-3">{result.totals.total_protein}</td>
+                                            </tr>
+                                        </>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     </div>
   );
